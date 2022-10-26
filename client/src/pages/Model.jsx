ed3882f43f021d9df89f33b95343f5ebc8427c41
JimmyTheFork <NeverSleepy567@gmail.com>
2022-10-26 22:12:43 +1300

    Classifier table is working

--- client/src/pages/Model.jsx
+++ client/src/pages/Model.jsx
@@ -7,10 +7,12 @@ import VariableSelector from "../components/VariableSelector";
 
 const Model = () => {
     const [setVar1, var1] = useState('defaultVariable');
+    const [tablesLoaded, setTablesLoaded] = useState(false);
     const [table1, setTable1] = useState(<HorizontalTable title="Classifiers" description="The following table will show basic Classification performance for the selected variable."
     labels={['Classifier', 'Accuracy']} data={[]} ></HorizontalTable>);
 
     useEffect(() => {
+        setTablesLoaded(false);
         let fd = new FormData()
         let file = new File([new Blob([window.data])],
             sessionStorage.getItem('raw_file_fileName'));
@@ -26,38 +28,53 @@ const Model = () => {
             res => res.json()
         ).then(
             data => {
-                let d = data[var1];
+                console.log(data);
+                // let d = data[var1];
 
                 setTable1(<HorizontalTable title="Classifiers"
                                            description="The following table will show basic Classification performance for the selected variable."
-                                           labels={['Classifier', 'Accuracy']} data={d}></HorizontalTable>);
+                                           labels={['Classifier', 'Accuracy']} data={
+                                           [[data.summarytable[0][0], data.summarytable[0][1].toFixed(2)],
+                                            [data.summarytable[1][0], data.summarytable[1][1].toFixed(2)],
+                                            [data.summarytable[2][0], data.summarytable[2][1].toFixed(2)],
+                                            [data.summarytable[3][0], data.summarytable[3][1].toFixed(2)]]}></HorizontalTable>);
 
+                setTablesLoaded(true)
             }
         );
-
+        
     }, [var1]);
 
     return (
         <div className='h-full'>
-            <div className='px-8 py-8 grid grid-cols-2 grid-rows-3 h-full gap-4'>
-                <div className='w-full h-full bg-gray-100 border border-gray-400 rounded-lg shadow-sm'>
-                    <div className='px-2 py-2'>
-                        <div className=" max-h-[400px]">
-                            <div className="shadow-lg">
-                                <VariableSelector setState={setVar1} label="Select Variable"></VariableSelector>
-                            </div>
-                            <div className="grid grid-rows-2 gap-2 mt-2">
-                                <div className="w-full text-left text-gray-700 max-h-[200px]" id="table1">{table1}</div>
-                            </div>
+           {/* Main grid */}
+           <div className="grid-flow-row-dense grid grid-rows-2 grid-cols-2 mx-4 my-4 gap-4 grow ">
+                {/* Variable Selector */}
+                <div className=" max-h-[400px] ">
+
+                    <div className="grid grid-rows-2 gap-2 mt-2 max-w-[455]">
+                        <div className="w-full " id="table1">
+                            {tablesLoaded? table1 : <HorizontalTable title="Classifiers" description="The following table will show basic Classification performance for the selected variable."
+                                                    labels={['Classifier', 'Accuracy']} data={[]} ></HorizontalTable>}
                         </div>
+
                     </div>
                 </div>
-                <div className='w-full h-full bg-gray-100 border border-gray-400 rounded-lg shadow-sm'>
-                    <div className='px-2 py-2'>Here goes forecasting</div>
+                
+                {/* Full Description */}
+                <div className="max-h-[600px]">
+                    <div className="grid grid-rows-2 gap-2 ">
+
+                    </div>
                 </div>
-                <div
-                    className='w-full h-full bg-gray-100 border border-gray-400 rounded-lg shadow-sm col-span-2 row-span-2'>
-                    <div className='px-2 py-2'>Here goes the graph</div>
+                
+
+
+                {/* Graph */}
+                <div className="shadow-md h-full border border-gray-400 bg-gray-100 col-span-2">
+                    <div id="graph" className="mx-4 my-4">
+
+                    </div>
                 </div>
 
             </div>
